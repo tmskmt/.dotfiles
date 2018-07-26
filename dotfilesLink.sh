fc01c601d5cb32acc494c5bd915be71c5491c7b7
kensuke.sakamoto <k.sakamoto@tcmobile.jp>
2018-07-26 09:55:22 +0900

    first commit:

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 
-ln -sf ~/dotfiles/.vim/template ~/.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/.vim/template ~/.vim
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.zsh_profile ~/.zsh_profile
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile

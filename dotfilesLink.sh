d2dd5d0a0ef710ef41922bd1b57f6415b618810a
kensuke.sakamoto <k.sakamoto@tcmobile.jp>
2018-09-12 15:24:21 +0900

    [change]各ディレクトリの表示化（.プレフィックスの削除）

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,4 @@
 ln -sf ~/.dotfiles/vim ~/.vim
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zsh_profile ~/.zsh_profile
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
